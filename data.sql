5fdb1c18ead34e5d1fb124f8d37ac66515e8b2a8
anqichen9856 <chenanqi9856@gmail.com>
2021-03-28 21:07:53 +0800

    reset data.sql; remove data.xlsx - use google sheets

--- data.sql
+++ data.sql
@@ -4,12 +4,3 @@ DATE: yyyy-mm-dd e.g. '1996-12-02'
 TIME: HH:MM e.g. '11:00'
 */
 
---Course_areas
-INSERT INTO TABLE Course_areas VALUES ('Computer Science', 36);
-INSERT INTO TABLE Course_areas VALUES ('Communications and New Media', 57);
-
---Courses
-INSERT INTO TABLE Courses VALUES ('CS2102', 'Database Systems', 'The aim of this module is to introduce the fundamental concepts and techniques necessary for the understanding and practice of design and implementation of database applications and of the management of data with relational database management systems.', 'Computer Science', 2);
-INSERT INTO TABLE Courses VALUES ('CS1101S', 'Programming Methodology', 'This module introduces the concepts of programming and computational problem solving, and is the first and foremost introductory module to computing.', 'Computer Science', 2);
-INSERT INTO TABLE Courses VALUES ('CSA6102', 'Cultural Studies in Asia', 'This module will examine the various areas of research in Cultural Studies conducted by Asian scholars or scholars locating their research in Asia.', 'Communications and New Media', 3);
-
