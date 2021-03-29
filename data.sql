babe7cd5cbe258d13d4ceae941ca9950cc72d685
anqichen9856 <chenanqi9856@gmail.com>
2021-03-29 12:21:54 +0800

    generate INSERT statements (in data.sql) based on data.xlsx

--- data.sql
+++ data.sql
@@ -1,6 +1,229 @@
-/* 
-INTEGER / NUMERIC: no ''
-DATE: yyyy-mm-dd e.g. '1996-12-02'
-TIME: HH:MM e.g. '11:00'
-*/
+--Employees
+INSERT INTO TABLE Employees VALUES (1, 'Tian Cai', 'abc@gmail.com', '(+65) 12345678', 'abc street', '2015-01-04', NULL);
+INSERT INTO TABLE Employees VALUES (2, 'Ben Dan', 'qqq@gmail.com', '(+65) 12345679', 'bcd street ', '2018-01-01', '2019-12-06');
+INSERT INTO TABLE Employees VALUES (3, 'Ha Ha', 'eee@gmail.com', '(+65) 12345680', 'def street', '2017-08-29', NULL);
+INSERT INTO TABLE Employees VALUES (4, 'John Lee', 'ooo@gmail.com', '(+65) 12345681', 'xxx street', '2016-01-03', NULL);
+INSERT INTO TABLE Employees VALUES (5, 'Alice Tan', 'hhh@gmail.com', '(+65) 12345682', 'yyy street', '2008-01-04', NULL);
+INSERT INTO TABLE Employees VALUES (6, 'Charles', 'halo@gmail.com', '(+65) 12345683', NULL, '2019-01-05', NULL);
+INSERT INTO TABLE Employees VALUES (7, 'David', 'hallo@gmail.com', '(+65) 12345684', 'woe street', '2009-01-06', '2021-03-05');
+INSERT INTO TABLE Employees VALUES (8, 'Frank', 'hello@gmail.com', '(+65) 12345685', 'wow street', '2018-01-07', NULL);
+INSERT INTO TABLE Employees VALUES (9, 'Bro', 'bye@gmail.com', '(+65) 12345686', NULL, '2020-01-08', NULL);
+INSERT INTO TABLE Employees VALUES (10, 'Hao De', 'byee@gmail.com', '(+65) 12345687', 'aha street', '2014-01-09', NULL);
+INSERT INTO TABLE Employees VALUES (11, 'Yi Yang Qian Xi', 'yyqx@163.com', '(+86) 123456789', 'tf building level 18', '2019-07-16', NULL);
+INSERT INTO TABLE Employees VALUES (12, 'Wang Junkai', 'wjk@163.com', '(+86) 123456700', 'tf building level 18', '2020-09-11', NULL);
+INSERT INTO TABLE Employees VALUES (13, 'Wang Yuan', 'wy@163.com', '(+86) 88888888', 'tf building level 18', '2020-04-05', '2021-03-07');
+INSERT INTO TABLE Employees VALUES (14, 'Ma Jiaqi', 'mjq@qq.com', '(+86) 13955581324', 'tf building level 18', '2021-03-28', NULL);
+INSERT INTO TABLE Employees VALUES (15, 'Hua Chenyu', 'hcy@outlook.com', '(+86) 123789456', 'tianyuchuanmei', '2019-03-14', NULL);
+INSERT INTO TABLE Employees VALUES (16, 'Joel', 'joelhong@gmail.com', '(+65) 90175679', NULL, '2017-09-30', '2018-04-01');
 
+--Full_time_Emp
+INSERT INTO TABLE Full_time_Emp VALUES (1, 10000);
+INSERT INTO TABLE Full_time_Emp VALUES (2, 9000);
+INSERT INTO TABLE Full_time_Emp VALUES (3, 8888);
+INSERT INTO TABLE Full_time_Emp VALUES (4, 4000);
+INSERT INTO TABLE Full_time_Emp VALUES (5, 5000);
+INSERT INTO TABLE Full_time_Emp VALUES (6, 600);
+INSERT INTO TABLE Full_time_Emp VALUES (7, 123456);
+INSERT INTO TABLE Full_time_Emp VALUES (8, 987);
+INSERT INTO TABLE Full_time_Emp VALUES (9, 100);
+INSERT INTO TABLE Full_time_Emp VALUES (10, 1987);
+
+--Part_time_Emp
+INSERT INTO TABLE Part_time_Emp VALUES (11, 2000);
+INSERT INTO TABLE Part_time_Emp VALUES (12, 80);
+INSERT INTO TABLE Part_time_Emp VALUES (13, 120);
+INSERT INTO TABLE Part_time_Emp VALUES (14, 70);
+INSERT INTO TABLE Part_time_Emp VALUES (15, 99);
+INSERT INTO TABLE Part_time_Emp VALUES (16, 86);
+
+--Instructors
+INSERT INTO TABLE Instructors VALUES (1);
+INSERT INTO TABLE Instructors VALUES (2);
+INSERT INTO TABLE Instructors VALUES (3);
+INSERT INTO TABLE Instructors VALUES (4);
+INSERT INTO TABLE Instructors VALUES (5);
+INSERT INTO TABLE Instructors VALUES (11);
+INSERT INTO TABLE Instructors VALUES (12);
+INSERT INTO TABLE Instructors VALUES (13);
+INSERT INTO TABLE Instructors VALUES (14);
+INSERT INTO TABLE Instructors VALUES (15);
+INSERT INTO TABLE Instructors VALUES (16);
+
+--Full_time_instructors
+INSERT INTO TABLE Full_time_instructors VALUES (1);
+INSERT INTO TABLE Full_time_instructors VALUES (2);
+INSERT INTO TABLE Full_time_instructors VALUES (3);
+INSERT INTO TABLE Full_time_instructors VALUES (4);
+INSERT INTO TABLE Full_time_instructors VALUES (5);
+
+--Part_time_instructors
+INSERT INTO TABLE Part_time_instructors VALUES (11);
+INSERT INTO TABLE Part_time_instructors VALUES (12);
+INSERT INTO TABLE Part_time_instructors VALUES (13);
+INSERT INTO TABLE Part_time_instructors VALUES (14);
+INSERT INTO TABLE Part_time_instructors VALUES (15);
+INSERT INTO TABLE Part_time_instructors VALUES (16);
+
+--Administrators
+INSERT INTO TABLE Administrators VALUES (9);
+INSERT INTO TABLE Administrators VALUES (10);
+
+--Managers
+INSERT INTO TABLE Managers VALUES (6);
+INSERT INTO TABLE Managers VALUES (7);
+INSERT INTO TABLE Managers VALUES (8);
+
+--Pay_slips
+
+--Rooms
+INSERT INTO TABLE Rooms VALUES (1, 'Room 1 at Level 1, Xiao Tiancai Building', 80);
+INSERT INTO TABLE Rooms VALUES (2, 'Room 2 at Level 1, Xiao Tiancai Building', 60);
+INSERT INTO TABLE Rooms VALUES (3, 'Lecture Theatre 1, Xiao Tiancai Building', 50);
+INSERT INTO TABLE Rooms VALUES (4, 'Room 3 at Level 1, Xiao Tiancai Building', 40);
+INSERT INTO TABLE Rooms VALUES (5, 'Room 4 at Level 1, Xiao Tiancai Building', 70);
+INSERT INTO TABLE Rooms VALUES (6, 'Room 5 at Level 1, Xiao Tiancai Building', 20);
+INSERT INTO TABLE Rooms VALUES (7, 'Room 1 at Level 2, Xiao Tiancai Building', 25);
+INSERT INTO TABLE Rooms VALUES (8, 'Lecture Theatre 2, Xiao Tiancai Building', 60);
+INSERT INTO TABLE Rooms VALUES (9, 'Room 3 at Level 3, Xiao Tiancai Building', 60);
+INSERT INTO TABLE Rooms VALUES (10, 'Classroom at Level 4, Xiao Tiancai Building', 30);
+INSERT INTO TABLE Rooms VALUES (11, 'Room 1 at Level 1, Da Bendan Building', 20);
+INSERT INTO TABLE Rooms VALUES (12, 'Room 2 at Level 1, Da Bendan Building', 50);
+INSERT INTO TABLE Rooms VALUES (13, 'Room 3 at Level 1, Da Bendan Building', 70);
+INSERT INTO TABLE Rooms VALUES (14, 'Room 1 at Level 2, Da Bendan Building', 60);
+INSERT INTO TABLE Rooms VALUES (15, 'Room 2 at Level 2, Da Bendan Building', 90);
+INSERT INTO TABLE Rooms VALUES (16, 'LT at Level 1, Da Bendan Building', 100);
+INSERT INTO TABLE Rooms VALUES (17, 'Room 3 at Level 2, Da Bendan Building', 40);
+INSERT INTO TABLE Rooms VALUES (18, 'Auditorium at Level 1, Da Bendan Building', 100);
+INSERT INTO TABLE Rooms VALUES (19, 'LT 2, Da Bendan Building', 80);
+INSERT INTO TABLE Rooms VALUES (20, 'LT 3, Da Bendan Building', 80);
+
+--Course_areas
+INSERT INTO TABLE Course_areas VALUES ('Computer Science', 6);
+INSERT INTO TABLE Course_areas VALUES ('Law', 6);
+INSERT INTO TABLE Course_areas VALUES ('Social Sciences', 6);
+INSERT INTO TABLE Course_areas VALUES ('Maths', 7);
+INSERT INTO TABLE Course_areas VALUES ('Chemistry', 7);
+INSERT INTO TABLE Course_areas VALUES ('Physics', 7);
+INSERT INTO TABLE Course_areas VALUES ('English Language', 8);
+INSERT INTO TABLE Course_areas VALUES ('Medicine', 8);
+
+--Courses
+INSERT INTO TABLE Courses VALUES (1, 'Database Systems', 'The aim of this module is to introduce the fundamental concepts and techniques necessary for the understanding and practice of design and implementation of database applications and of the management of data with relational database management systems.', 'Computer Science', 2.0);
+INSERT INTO TABLE Courses VALUES (2, 'Programming Methodology', 'This module introduces the concepts of programming and computational problem solving, and is the first and foremost introductory module to computing.', 'Computer Science', 2.0);
+INSERT INTO TABLE Courses VALUES (3, 'Cultural Studies in Asia', 'This module will examine the various areas of research in Cultural Studies conducted by Asian scholars or scholars locating their research in Asia.', 'Social Sciences', 3.0);
+INSERT INTO TABLE Courses VALUES (4, 'Maritime Conflict of Laws', 'An examination of conflict of laws issues in the context of maritime law and admiralty litigation. ', 'Law', 2.0);
+INSERT INTO TABLE Courses VALUES (5, 'Linear Algebra', 'This module is a first course in linear algebra. Fundamental concepts of linear algebra will be introduced and investigated in the context of the Euclidean spaces R^n. ', 'Maths', 1.5);
+INSERT INTO TABLE Courses VALUES (6, 'Calculus', 'This is a course in single-variable calculus. ', 'Maths', 1.0);
+INSERT INTO TABLE Courses VALUES (7, 'Quantum Optics', 'This module is an introduction to the quantum description of the electromagnetic field, with a special focus on phenomena at optical frequencies.', 'Physics', 2.0);
+INSERT INTO TABLE Courses VALUES (8, 'Fluid Dynamics', 'This module introduces physics students to the fundamental aspects of fluid dynamics. ', 'Physics', 1.0);
+INSERT INTO TABLE Courses VALUES (9, 'General Chemistry', 'This is a chemistry module designed for non-chemistry majors and deals primarily with basic principles of structure and bonding, thermodynamics, kinetics, basic analytical techniques, properties and reactions of organic functional groups and chemistry of main group elements.', 'Chemistry', 1.5);
+INSERT INTO TABLE Courses VALUES (10, 'Structure of Sentences and Meanings', 'This module explores language structure, in particular, patterns of sentence structure (syntax) and of meaning (semantics) in English. ', 'English Language and Literature', 1.0);
+
+--Offerings
+INSERT INTO TABLE Offerings VALUES (1, '2020-09-01', '2020-10-01', '2020-10-05', '2020-09-20', 100, 160, 39.9, 9);
+INSERT INTO TABLE Offerings VALUES (2, '2020-10-05', '2020-12-01', '2020-12-20', '2020-11-15', 20, 40, 29.9, 9);
+INSERT INTO TABLE Offerings VALUES (2, '2021-01-01', '2021-02-01', '2021-02-02', '2021-01-15', 50, 80, 39.9, 9);
+INSERT INTO TABLE Offerings VALUES (3, '2021-03-10', '2021-04-10', '2021-04-15', '2021-03-30', 80, 100, 59.9, 9);
+INSERT INTO TABLE Offerings VALUES (4, '2020-09-01', '2020-10-01', '2020-10-05', '2020-09-20', 200, 200, 10.9, 9);
+INSERT INTO TABLE Offerings VALUES (4, '2021-03-10', '2021-04-10', '2021-04-15', '2021-03-30', 200, 200, 30.9, 10);
+INSERT INTO TABLE Offerings VALUES (5, '2020-05-01', '2020-06-01', '2020-06-10', '2020-05-20', 150, 160, 49.9, 10);
+INSERT INTO TABLE Offerings VALUES (5, '2021-01-01', '2021-02-01', '2021-02-03', '2020-01-15', 150, 160, 49.9, 10);
+INSERT INTO TABLE Offerings VALUES (5, '2021-03-10', '2021-04-10', '2021-04-15', '2021-03-30', 180, 180, 39.9, 10);
+INSERT INTO TABLE Offerings VALUES (6, '2021-02-02', '2021-02-25', '2021-02-28', '2021-02-15', 60, 160, 35.9, 10);
+
+--Sessions
+INSERT INTO TABLE Sessions VALUES (1, '2020-09-01', 1, '2020-10-01', 9.0, 11.0, 1, 1);
+INSERT INTO TABLE Sessions VALUES (1, '2020-09-01', 2, '2020-10-05', 9.0, 11.0, 1, 1);
+INSERT INTO TABLE Sessions VALUES (2, '2020-10-05', 1, '2020-12-01', 14.0, 16.0, 2, 6);
+INSERT INTO TABLE Sessions VALUES (2, '2020-10-05', 2, '2020-12-20', 14.0, 16.0, 2, 6);
+INSERT INTO TABLE Sessions VALUES (2, '2021-01-01', 1, '2021-02-01', 14.0, 16.0, 1, 8);
+INSERT INTO TABLE Sessions VALUES (2, '2021-01-01', 2, '2021-02-02', 14.0, 16.0, 1, 8);
+INSERT INTO TABLE Sessions VALUES (3, '2021-03-10', 1, '2021-04-10', 14.0, 17.0, 11, 3);
+INSERT INTO TABLE Sessions VALUES (3, '2021-03-10', 2, '2021-04-15', 14.0, 17.0, 11, 3);
+INSERT INTO TABLE Sessions VALUES (4, '2020-09-01', 1, '2020-10-01', 15.0, 17.0, 3, 16);
+INSERT INTO TABLE Sessions VALUES (4, '2020-09-01', 2, '2020-10-05', 9.0, 11.0, 4, 16);
+INSERT INTO TABLE Sessions VALUES (4, '2021-03-10', 1, '2021-04-10', 13.0, 15.0, 5, 16);
+INSERT INTO TABLE Sessions VALUES (4, '2021-03-10', 2, '2021-04-15', 16.0, 18.0, 4, 16);
+INSERT INTO TABLE Sessions VALUES (5, '2020-05-01', 1, '2020-06-01', 9.0, 10.5, 12, 20);
+INSERT INTO TABLE Sessions VALUES (5, '2020-05-01', 2, '2020-06-10', 10.0, 11.5, 12, 19);
+INSERT INTO TABLE Sessions VALUES (5, '2021-01-01', 1, '2021-02-01', 13.0, 14.5, 12, 20);
+INSERT INTO TABLE Sessions VALUES (5, '2021-01-01', 2, '2021-02-03', 13.0, 14.5, 12, 19);
+INSERT INTO TABLE Sessions VALUES (5, '2021-03-10', 1, '2021-04-10', 14.0, 15.5, 12, 20);
+INSERT INTO TABLE Sessions VALUES (5, '2021-03-10', 2, '2021-04-15', 15.5, 17.0, 12, 18);
+INSERT INTO TABLE Sessions VALUES (6, '2021-02-02', 1, '2021-02-25', 9.0, 10.0, 12, 20);
+INSERT INTO TABLE Sessions VALUES (6, '2021-02-02', 2, '2021-02-28', 17.0, 18.0, 12, 19);
+
+--Specializes
+INSERT INTO TABLE Specializes VALUES (1, 'Computer Science');
+INSERT INTO TABLE Specializes VALUES (2, 'Computer Science');
+INSERT INTO TABLE Specializes VALUES (3, 'Law');
+INSERT INTO TABLE Specializes VALUES (4, 'Law');
+INSERT INTO TABLE Specializes VALUES (5, 'Law');
+INSERT INTO TABLE Specializes VALUES (11, 'Social Sciences');
+INSERT INTO TABLE Specializes VALUES (12, 'Maths');
+INSERT INTO TABLE Specializes VALUES (13, 'Chemistry');
+INSERT INTO TABLE Specializes VALUES (14, 'Physics');
+INSERT INTO TABLE Specializes VALUES (15, 'English Language');
+INSERT INTO TABLE Specializes VALUES (16, 'Medicine');
+
+--Customers
+INSERT INTO TABLE Customers VALUES (1, 'Xiao Ming', 'xm@gmail.com', '(+86) 11111111', 'Changyang Street');
+INSERT INTO TABLE Customers VALUES (2, 'Xiao Wang', 'xw@gmail.com', '(+86) 11111112', 'Sanxing Street');
+INSERT INTO TABLE Customers VALUES (3, 'Xiao Zhang', 'xz@gmail.com', '(+86) 11111113', 'Yangpu Street');
+INSERT INTO TABLE Customers VALUES (4, 'Zhang Fei', 'abc123@gmail.com', '(+86) 11111114', 'Shu Street');
+INSERT INTO TABLE Customers VALUES (5, 'Liu Bei', 'aaa123@gmail.com', '(+86) 11111115', 'Shu Street');
+INSERT INTO TABLE Customers VALUES (6, 'Guan Yu', 'xyz999@gmail.com', '(+86) 11111116', 'Shu Street');
+INSERT INTO TABLE Customers VALUES (7, 'Cao Cao', 'qwq123@gmail.com', '(+86) 11111117', 'Wei Street');
+INSERT INTO TABLE Customers VALUES (8, 'Sun Quan', 'www123@gmail.com', '(+86) 11111118', 'Wu Street');
+INSERT INTO TABLE Customers VALUES (9, 'Ling Daiyu', 'ld@gmail.com', '(+86) 11111119', 'Daguanyuan Street');
+INSERT INTO TABLE Customers VALUES (10, 'Jia Baoyu', 'abcd@gmail.com', '(+86) 11111120', 'Daguanyuan Street');
+
+--Credit_cards
+INSERT INTO TABLE Credit_cards VALUES ('A123456789012', '2024-07-03', 123);
+INSERT INTO TABLE Credit_cards VALUES ('A123456789033', '2024-07-04', 456);
+INSERT INTO TABLE Credit_cards VALUES ('A123456789014', '2024-07-05', 788);
+INSERT INTO TABLE Credit_cards VALUES ('A123456789015', '2024-07-06', 233);
+INSERT INTO TABLE Credit_cards VALUES ('A123456789016', '2024-07-07', 666);
+INSERT INTO TABLE Credit_cards VALUES ('A123456789017', '2024-07-08', 777);
+INSERT INTO TABLE Credit_cards VALUES ('A123456789018', '2024-07-09', 888);
+INSERT INTO TABLE Credit_cards VALUES ('A123456789019', '2024-07-10', 999);
+INSERT INTO TABLE Credit_cards VALUES ('A123456789020', '2024-07-11', 555);
+INSERT INTO TABLE Credit_cards VALUES ('A123456789021', '2024-07-12', 100);
+
+--Owns
+INSERT INTO TABLE Owns VALUES (1, 'A123456789012', '2019-07-03');
+INSERT INTO TABLE Owns VALUES (2, 'A123456789033', '2019-07-04');
+INSERT INTO TABLE Owns VALUES (3, 'A123456789014', '2019-07-05');
+INSERT INTO TABLE Owns VALUES (4, 'A123456789015', '2019-07-06');
+INSERT INTO TABLE Owns VALUES (5, 'A123456789016', '2019-07-07');
+INSERT INTO TABLE Owns VALUES (6, 'A123456789017', '2019-07-08');
+INSERT INTO TABLE Owns VALUES (7, 'A123456789018', '2019-07-09');
+INSERT INTO TABLE Owns VALUES (8, 'A123456789019', '2019-07-10');
+INSERT INTO TABLE Owns VALUES (9, 'A123456789020', '2019-07-11');
+INSERT INTO TABLE Owns VALUES (10, 'A123456789021', '2019-07-12');
+
+--Course_packages
+INSERT INTO TABLE Course_packages VALUES (1, 6, '2016-06-06', '2016-12-26', 'Wu Di 666', 166);
+INSERT INTO TABLE Course_packages VALUES (2, 8, '2018-08-08', '2028-08-08', 'How To Get Rich', 178);
+INSERT INTO TABLE Course_packages VALUES (3, 20, '2019-01-01', '2029-12-31', 'Forever Young', 233);
+INSERT INTO TABLE Course_packages VALUES (4, 22, '2021-01-01', '2022-02-02', 'Te Hui ', 399);
+
+--Buys
+INSERT INTO TABLE Buys VALUES (1, 'A123456789012', '2016-06-07', 6);
+INSERT INTO TABLE Buys VALUES (2, 'A123456789016', '2018-08-08', 6);
+INSERT INTO TABLE Buys VALUES (3, 'A123456789012', '2022-01-20', 20);
+INSERT INTO TABLE Buys VALUES (3, 'A123456789021', '2022-02-01', 20);
+INSERT INTO TABLE Buys VALUES (3, 'A123456789033', '2022-02-02', 20);
+
+--Redeems
+INSERT INTO TABLE Redeems VALUES (2, 'A123456789016', '2018-08-08', 6, '2021-02-02', 1, '2018-08-10');
+INSERT INTO TABLE Redeems VALUES (2, 'A123456789018', '2018-08-09', 4, '2020-09-01', 1, '2020-09-10');
+
+--Registers
+INSERT INTO TABLE Registers VALUES ('A123456789012', 1, '2020-09-01', 2, '2020-09-22');
+INSERT INTO TABLE Registers VALUES ('A123456789033', 2, '2020-10-05', 1, '2020-12-01');
+INSERT INTO TABLE Registers VALUES ('A123456789021', 3, '2021-03-10', 1, '2021-04-08');
+
+--Cancels
+INSERT INTO TABLE Cancels VALUES ('A123456789012', 1, '2020-09-01', 2, '2020-09-28', 39.9, 0);
+INSERT INTO TABLE Cancels VALUES ('A123456789018', 4, '2020-09-01', 1, '2020-09-12', 0.0, 1);
--- generate_insert_stmts.py
+++ generate_insert_stmts.py
@@ -5,17 +5,23 @@ def make_values(row, n):
     l = []
     for i in range(n):
         val = str(row[i])
-        if val == '':
+        if val == '' or val == 'nan' or val == 'NaT':
             l.append('NULL')
-        elif re.match(r'[0-9]+(\.[0-9]+)*', val):
+        elif re.match(r'^[0-9]+(\.[0-9]+)*$', val):
             l.append(val)
+        elif ' 00:00:00' in val:
+            l.append('\'' + val[:-9] + '\'')
         else:
             l.append('\'' + val + '\'')
     return ', '.join(l)
 
 def make_stmts_for_table(df, name):
     l = ['--' + name]
-    n = len(df. columns)
+    n = len(df.columns)
+    # for i, col in enumerate(df.columns):
+    #     if 'Unnamed' in col:
+    #         n = i
+    #         break
     for index, row in df.iterrows():
         values = make_values(row, n)
         l.append('INSERT INTO TABLE ' + name + ' VALUES (' + values + ');')
@@ -24,6 +30,10 @@ def make_stmts_for_table(df, name):
 workbook = pd.read_excel('data.xlsx', sheet_name=None, engine='openpyxl')
 f = open('data.sql', 'a')
 for name, df in workbook.items():
+    print(name)
     f.write(make_stmts_for_table(df, name) + '\n\n')
-    
-    
+
+# For individual sheet testing    
+# df = pd.read_excel('data.xlsx', sheet_name='Cancels', engine='openpyxl')
+# f = open('data.sql', 'a')
+# f.write(make_stmts_for_table(df, 'Test') + '\n\n')
