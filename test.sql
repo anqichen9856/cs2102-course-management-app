332d4ca11938c0dfdc8d31af1c2d2787f044e9e7
Yufei <hellobluethroat@gmail.com>
2021-04-10 15:55:47 +0800

    test 19-24
    21 needs some instructor specializes in that area

--- test.sql
+++ test.sql
@@ -1,1 +1,29 @@
 -- Test cases for routines
+-- test 19
+-- CALL update_course_session (12, 10, '2021-03-01', 9);
+-- CALL update_course_session (12, 10, '2021-03-01', 8);
+
+-- test 20
+-- CALL cancel_registration (12, 10, '2021-03-01');
+
+-- test 21
+-- need some instructor teaches same area
+-- CALL update_instructor (10, '2021-03-01', 8, new_eid INTEGER);
+
+-- test 22
+-- CALL update_room (10, '2021-03-01', 8, 16);
+-- CALL update_room (10, '2021-03-01', 10, 20);
+-- CALL update_room (10, '2021-03-01', 10, 19);
+
+
+-- test 23
+-- CALL remove_session (10, '2021-03-01', 9);
+-- CALL remove_session (10, '2021-03-01', 7);
+-- CALL remove_session (10, '2021-03-01', 6);
+
+
+-- test 24
+-- CALL add_session (11, '2021-05-01', 2, '2021-09-06', 9.0, 3, 1);
+-- CALL add_session (11, '2021-05-01', 3, '2021-09-07', 9.0, 3, 1);
+-- CALL add_session (11, '2021-05-01', 4, '2021-09-08', 9.0, 3, 1);
+-- CALL add_session (11, '2021-05-01', 5, '2021-09-09', 9.0, 3, 1);
