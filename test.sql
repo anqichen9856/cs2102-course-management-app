cf04ef5951e455f8404dcc686b0c127642eebe5a
Yufei <hellobluethroat@gmail.com>
2021-04-10 20:02:43 +0800

    delete comments

--- test.sql
+++ test.sql
@@ -63,7 +63,7 @@ SELECT * FROM find_instructors (1, '2021-03-31', 9);
 
 select * from courses where course_area = 'Artificial Intelligence'
 select * from specializes where area = 'Artificial Intelligence'
-select * from sessions where eid = 3 
+select * from sessions where eid = 3
 -- instructor 3 has another session
 SELECT * FROM find_instructors (3, '2021-04-09', 14);
 SELECT * FROM find_instructors (11, '2021-04-09', 9);
@@ -95,10 +95,12 @@ SELECT * FROM find_rooms('2021-04-10', 14, 3);
 SELECT * FROM find_rooms('2021-04-9', 17, 2);
 
 -- 9
--- 04-08 all available, 04-09 3,16,20,21 not avaialble 
+-- 04-08 all available, 04-09 3,16,20,21 not avaialble
 SELECT * FROM get_available_rooms('2021-04-08', '2021-04-09');
 -- skip weekends
 SELECT * FROM get_available_rooms('2021-04-09', '2021-04-10');
+
+
 
 -- 10
 -- successul insertion
@@ -109,10 +111,12 @@ SELECT * FROM Sessions WHERE cid = 10 AND launch_date = '2021-06-01';
 CALL add_course_offering(10, 10, DATE '2021-06-02', '2021-07-01', 2, 10, '{{"2021-07-05", "14", "21"}, {"2021-07-06", "14", "21"}}');
 -- sessions over weekends
 CALL add_course_offering(10, 10, DATE '2021-06-02', '2021-07-01', 2, 10, '{{"2021-07-04", "14", "21"}, {"2021-07-06", "14", "21"}}');
--- check by routine: seating capacity 
+-- check by routine: seating capacity
 CALL add_course_offering(10, 10, DATE '2021-06-02', '2021-06-20', 3, 10, '{{"2021-07-05", "14", "21"}, {"2021-07-06", "14", "21"}}');
 -- check by trigger: room, instructor availablility, overlap of sessions
 -- to be demoed by add_session
+
+
 
 -- 11
 CALL add_course_package('Cheapest package', 1, '2021-05-01', '2021-05-03', 10);
@@ -137,6 +141,9 @@ CALL buy_course_package(11, 1);
 -- Case 4: customer 12 successfully buys package 2
 CALL buy_course_package(11, 2);
 SELECT * FROM Buys;
+-- CALL buy_course_package(1,1);
+-- SELECT * FROM Buys
+
 
 -- 14
 SELECT * FROM get_my_course_package(5);
@@ -145,15 +152,21 @@ SELECT * FROM get_my_course_package(5);
 -- SELECT * FROM Redeems;
 -- SELECT * FROM Cancels;
 
+
 -- 15
 -- By default, three course offerings are returned.
 SELECT * FROM get_available_course_offerings();
+
 
 -- 16
 -- Case 1: registration deadline is passed
 SELECT * FROM get_available_course_sessions(1, '2020-09-01');
 -- Case 2: by default, three sessions of course 7 are returned.
 SELECT * FROM get_available_course_sessions(7, '2021-03-30');
+-- the customer not register or redeem any session or canceled
+-- CALL cancel_registration (2, 10, '2021-03-01');
+-- Cancellation will be proceed, but the number of remaining redemptions in your package cannot be added back
+-- CALL cancel_registration (5, 10, '2021-03-01');
 
 -- 17
 -- Case 1: register SUCCESS
@@ -163,44 +176,60 @@ SELECT * FROM Registers;
 CALL register_session(3, 7, '2021-03-30', 1, 1);
 SELECT * FROM Redeems;
 
+
+
 -- 18
 SELECT * FROM get_my_registrations(1);
 
 
 -- test 19
--- successful:
 CALL update_course_session (12, 10, '2021-03-01', 9);
 CALL update_course_session (12, 10, '2021-03-01', 8);
+-- session is not avaliable:
+-- CALL update_course_session (12, 10, '2021-03-01', 100);
+-- this customer is not exist
+-- CALL update_course_session (120, 10, '2021-03-01', 8);
+-- the custommer does not have a registerd session for this course
+-- CALL update_course_session (1, 10, '2021-03-01', 8);
+-- no seat in new session
+-- CALL update_course_session (12, 10, '2021-03-01', 5);
+
+
 
 -- test 20
-
 CALL cancel_registration (12, 10, '2021-03-01');
+case Session started:
+CALL cancel_registration (11, 10, '2021-03-01');
+-- the customer not register or redeem any session or canceled
+-- CALL cancel_registration (2, 10, '2021-03-01');
+-- Cancellation will be proceed, but the number of remaining redemptions in your package cannot be added back
+-- CALL cancel_registration (5, 10, '2021-03-01');
+
 
 -- test 21
--- successful:
--- need some instructor teaches same area
--- CALL update_instructor (10, '2021-03-01', 8, new_eid INTEGER);
+CALL update_instructor (11, '2021-05-01', 1, 21);
+CALL update_instructor (11, '2021-05-01', 2, 21);
 
 -- test 22
--- successful:
 CALL update_room (10, '2021-03-01', 8, 16);
 CALL update_room (10, '2021-03-01', 10, 20);
 CALL update_room (10, '2021-03-01', 10, 19);
 
 
 -- test 23
--- successful:
 CALL remove_session (10, '2021-03-01', 9);
 CALL remove_session (10, '2021-03-01', 7);
 CALL remove_session (10, '2021-03-01', 6);
 
 
 -- test 24
--- successful:
 CALL add_session (11, '2021-05-01', 2, '2021-09-06', 9.0, 3, 1);
 CALL add_session (11, '2021-05-01', 3, '2021-09-07', 9.0, 3, 1);
 CALL add_session (11, '2021-05-01', 4, '2021-09-08', 9.0, 3, 1);
 CALL add_session (11, '2021-05-01', 5, '2021-09-09', 9.0, 3, 1);
+-- session overlap:
+-- CALL add_session (3, '2021-04-30', 2, '2021-06-22', 15, 21, 1);
+
 
 -- 25
 delete from pay_slips where EXTRACT(MONTH FROM payment_date) = 4;
@@ -223,12 +252,12 @@ WITH Reg AS (
     UNION
     (select cust_id, date from Customers NATURAL JOIN Owns NATURAL JOIN Redeems)
 )
-select distinct cust_id from Reg where date >= '2020-10-01' order by cust_id 
+select distinct cust_id from Reg where date >= '2020-10-01' order by cust_id
 
 -- open offerings
 select * from offerings where registration_deadline >= CURRENT_DATE
 
--- Cho Chang (15) has some interest area: register before 6 months 
+-- Cho Chang (15) has some interest area: register before 6 months
 -- prev 6 months: 2020-10 to 2021-04
 select cust_id, course_area, date from Customers NATURAL JOIN Owns NATURAL JOIN Registers NATURAL JOIN Courses where cust_id = 15
 
@@ -249,15 +278,3 @@ SELECT * FROM view_summary_report(20);
 
 -- 30
 SELECT * FROM view_manager_report();
-
-
-
-
-
-
-
-
-
-
-
-
