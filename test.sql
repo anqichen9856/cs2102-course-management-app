7c6db0a7086c488facbf60a5e687e6197eae6d2d
anqichen9856 <chenanqi9856@gmail.com>
2021-04-10 18:34:33 +0800

    写完test cases, 加了一个register

--- test.sql
+++ test.sql
@@ -11,40 +11,66 @@ $$;
 (VALUES (row), (row))
 
 -- 1
+select * from employees
+select * from administrators
+select * from managers
+select * from full_time_instructors
+select * from part_time_instructors
+select * from specializes
+select * from course_areas
 -- Full-time, admin, course area = 0
 CALL add_employee ('Jasmine Ang', '12 Kent Ridge Drive', '(+65) 90176780', 'jasmine@gmail.com', 'monthly', '1200', '2021-03-31', 'administrator', '{}');
 -- Full-time instructor, course area >= 1
-CALL add_employee ('Richard Xiong', '12 Kent Ridge Drive', '(+65) 90176780', 'rx@gmail.com', 'monthly', '3000', '2021-03-31', 'instructor', '{"Computer Science"}');
+CALL add_employee ('Richard Xiong', '12 Kent Ridge Drive', '(+65) 90176780', 'rx@gmail.com', 'monthly', '3000', '2021-03-31', 'instructor', '{"Parallel Computing"}');
 -- Part-time instructor, course area >= 1
-CALL add_employee ('Joyce Chua', '12 Kent Ridge Drive', '(+65) 90176780', 'joyce@gmail.com', 'hourly', '50.90', '2021-03-20', 'instructor', '{"Social Sciences", "Law"}');
+CALL add_employee ('Joyce Chua', '12 Kent Ridge Drive', '(+65) 90176780', 'joyce@gmail.com', 'hourly', '50.90', '2021-03-20', 'instructor', '{"Computer Security", "Algorithms & Theory"}');
 -- Full-time manager, course area >= 0
-CALL add_employee ('Wang Qian', '12 Kent Ridge Drive', '(+65) 90176780', 'qiannnnw@gmail.com', 'monthly', '2400', '2021-03-20', 'manager', '{"Law"}');
+CALL add_employee ('Wang Qian', '12 Kent Ridge Drive', '(+65) 90176780', 'qiannnnw@gmail.com', 'monthly', '2400', '2021-03-20', 'manager', '{"Artificial Intelligence"}');
 -- Full-time manager, course area >= 0
 CALL add_employee ('Bryan Wang', '12 Kent Ridge Drive', '(+65) 90176780', 'bryanwang@gmail.com', 'monthly', '2400', '2021-03-20', 'manager', '{}');
 
 -- 2
-CALL remove_employee (3, '2021-02-29');
+select * from sessions where eid = 3
+select * from employees
+select * from instructors
+CALL remove_employee (3, '2021-02-28');
 CALL remove_employee (21, CURRENT_DATE);
 
 -- 3
+select * from customers
+select * from credit_cards
+select * from owns
 CALL add_customer ('Chen Anqi', '3 Jurong East Street 32', '(+65) 90174780', 'anqichen@gmail.com', 'A0188533W1234', '2023-02-20', 886);
 
 -- 4
+select * from owns where cust_id=11
+select * from credit_cards
 CALL update_credit_card (11, 'A0188533119W0117', '2026-09-27', 901);
 
 -- 5
+select * from courses
 CALL add_course ('Wireless Networking', 'This module aims to provide solid foundation for students in the area of wireless networks and introduces students to the emerging area of cyber-physical-system/Internet-of-Things.',
 'Networking', 2.5);
 
 -- 6
 -- Database Systems: course=1, instructor=1,20
--- Artificial Intelligence: course=2,3,11, instructor=3,21
+-- Artificial Intelligence: course=3,11, instructor=3,21
 -- Software Engineering: course=10, instructor=15,17
+
+select * from courses where course_area = 'Database Systems'
+select * from specializes where area = 'Database Systems'
 SELECT * FROM find_instructors (1, '2021-03-31', 9);
-SELECT * FROM find_instructors (2, '2021-03-31', 10);
-SELECT * FROM find_instructors (3, '2021-03-31', 14);
+
+select * from courses where course_area = 'Artificial Intelligence'
+select * from specializes where area = 'Artificial Intelligence'
+select * from sessions where eid = 3 
+-- instructor 3 has another session
+SELECT * FROM find_instructors (3, '2021-04-09', 14);
+SELECT * FROM find_instructors (11, '2021-04-09', 9);
+
+select * from courses where course_id = 10
+-- 15+4=19 over time
 SELECT * FROM find_instructors (10, '2021-03-31', 15);
-SELECT * FROM find_instructors (11, '2021-03-31', 16);
 
 -- 13
 -- Case 1: unable to buy package 2 because customer 1 has an active package
@@ -115,15 +141,40 @@ SELECT * FROM get_my_registrations(1);
 -- CALL add_session (11, '2021-05-01', 4, '2021-09-08', 9.0, 3, 1);
 -- CALL add_session (11, '2021-05-01', 5, '2021-09-09', 9.0, 3, 1);
 
+-- 25
+delete from pay_slips where EXTRACT(MONTH FROM payment_date) = 4;
+SELECT * FROM pay_salary() order by status;
+
+select * from pay_slips
+-- dio brando not full: 6/30*470=94
+select * from employees where eid = 2
+select * from employees where eid = 21
+-- 28 hours for eid 15
+select sum(end_time - start_time) from sessions where eid=15 and EXTRACT(MONTH FROM date) = 4
+
+-- 26
+SELECT * FROM promote_courses();
+select cust_id from customers except SELECT cust_id FROM promote_courses() order by cust_id
+-- active customers
+-- 1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 21
+WITH Reg AS (
+    (select cust_id, date from Customers NATURAL JOIN Owns NATURAL JOIN Registers)
+    UNION
+    (select cust_id, date from Customers NATURAL JOIN Owns NATURAL JOIN Redeems)
+)
+select distinct cust_id from Reg where date >= '2020-10-01' order by cust_id 
+
+-- open offerings
+select * from offerings where registration_deadline >= CURRENT_DATE
+
+-- 15 Cho Chang has some interest area: register before 6 months 
+-- 6 months: 2020-10 to 2021-04
+select cust_id, course_area, date from Customers NATURAL JOIN Owns NATURAL JOIN Registers NATURAL JOIN Courses where cust_id = 15
+
 
 -- 29 
 SELECT * FROM view_summary_report(20);
 
 
 
--- 25
--- DELETE FROM Pay_slips;
-SELECT * FROM pay_salary();
 
--- 26
-SELECT * FROM promote_courses();
