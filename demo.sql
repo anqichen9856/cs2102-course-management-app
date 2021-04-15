3eed26aaab7ac37f182cf7d12173b9c2ae89708d
Yufei <hellobluethroat@gmail.com>
2021-04-15 13:12:26 +0800

    Update demo.sql

--- demo.sql
+++ demo.sql
@@ -53,31 +53,27 @@ SELECT * FROM find_rooms('2021-04-10', 14, 3);
 
 
 -- test 20
--- SELECT * FROM Cancels;
--- SELECT * FROM Redeems;
 -- SELECT * FROM Registers;
 -- SELECT * FROM Owns;
 
+-- SELECT * FROM Cancels;
 -- case 1: cancel successfully(customer 12 cancels course 10, from redeem)
--- SELECT * FROM Buys;
--- CALL cancel_registration (12, 10, '2021-03-01');
+-- SELECT * FROM Redeems WHERE card_number = 'A123456789023';
+-- SELECT * FROM Buys WHERE card_number = 'A123456789023' AND package_id = 8;
+-- CALL cancel_registration (12, 10, '2021-03-01'); -- 10
 -- SELECT * FROM Cancels;
--- SELECT * FROM Buys;
+-- SELECT * FROM Buys WHERE card_number = 'A123456789023' AND package_id = 8;
 
--- CALL register_session (1, 10, '2021-03-01', 10, 0); -- register
--- SELECT * FROM Registers;
+
+-- CALL register_session (3, 5, '2021-03-30', 1, 0); -- register
+-- SELECT * FROM Registers WHERE course_id = 5 AND launch_date = '2021-03-30';
 -- case 2: cancel successfully(customer 12 cancels course 10, register directly)
--- CALL cancel_registration (1, 10, '2021-03-01');
+-- CALL cancel_registration (3, 5, '2021-03-30');
 -- SELECT * FROM Cancels;
+-- SELECT fees FROM Offerings WHERE course_id = 5 AND launch_date = '2021-03-30';
 
 -- case 3: the customer not register or redeem any session or canceled
 -- CALL cancel_registration (1, 10, '2021-03-01');
 
 -- case 4: Session started:
 -- CALL cancel_registration (11, 10, '2021-03-01');
-
--- pass the deadline
--- CALL add_session (3, '2021-04-18', 3, '2021-04-20', 14, 3, 3);
--- CALL register_session (3, 10, '2021-03-01', 5, 0);
--- SELECT * FROM Registers;
--- SELECT * FROM Owns;
